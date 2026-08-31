0ecf1221bdd6ed6c287a9cd6997e90ef6e235d1d
sakshi Pal <sakshi.pal2815@gmail.com>
2026-08-31 13:38:48 +0530

    resolve merge conflict

--- backend/src/modules/user/user.service.js
+++ backend/src/modules/user/user.service.js
@@ -1,77 +1,7 @@
-<<<<<<< HEAD
-const User = require("./user.model");
-
-const getUserProfile = async (userId) => {
-  const user = await User.findById(userId);
-
-  if (!user) {
-    const error = new Error("User not found");
-    error.statusCode = 404;
-    throw error;
-  }
-
-  return user.toSafeObject();
-};
-
-const updateUserProfile = async (userId, profileData) => {
-  const user = await User.findById(userId);
-
-  if (!user) {
-    const error = new Error("User not found");
-    error.statusCode = 404;
-    throw error;
-  }
-
-  const allowedFields = [
-    "name",
-    "headline",
-    "bio",
-    "location",
-    "phone",
-    "website",
-    "profileImage",
-  ];
-
-  allowedFields.forEach((field) => {
-    if (profileData[field] !== undefined) {
-      user[field] =
-        typeof profileData[field] === "string"
-          ? profileData[field].trim()
-          : profileData[field];
-    }
-  });
-
-  if (profileData.socialLinks !== undefined) {
-    user.socialLinks = {
-      github:
-        profileData.socialLinks.github !== undefined
-          ? profileData.socialLinks.github.trim()
-          : user.socialLinks?.github || "",
-
-      linkedin:
-        profileData.socialLinks.linkedin !== undefined
-          ? profileData.socialLinks.linkedin.trim()
-          : user.socialLinks?.linkedin || "",
-
-      twitter:
-        profileData.socialLinks.twitter !== undefined
-          ? profileData.socialLinks.twitter.trim()
-          : user.socialLinks?.twitter || "",
-    };
-  }
-
-  await user.save();
-
-  return user.toSafeObject();
-};
-
-module.exports = {
-  getUserProfile,
-  updateUserProfile,
-=======
-import { findUserById, updateUserById, } from "./user.repository.js";
+import { findUserById, updateUserById } from "./user.repository.js";
 import ApiError from "../../utils/ApiError.js";
 
+// Get User Profile
 export const getUserProfile = async (userId) => {
   const user = await findUserById(userId);
 
@@ -83,8 +13,7 @@ export const getUserProfile = async (userId) => {
 };
 
 
-
-// Update Profile
+// Update User Profile
 export const updateUserProfile = async (userId, updateData) => {
   const user = await findUserById(userId);
 
@@ -95,5 +24,4 @@ export const updateUserProfile = async (userId, updateData) => {
   const updatedUser = await updateUserById(userId, updateData);
 
   return updatedUser;
->>>>>>> 2ce68f34ee245e8af9c2d846267b7f2c54cebd7c
 };
